6e27c911d3e16e6f1b0237e36c00e1dcb45f6bb0
Kenny Grant <kenny@mech.design>
2024-11-06 08:32:10 +0000

    Change resize image dependency

--- file/image.go
+++ file/image.go
@@ -1,7 +1,6 @@
 package file
 
 import (
-	"errors"
 	"fmt"
 	"image"
 	"image/jpeg"
@@ -12,9 +11,10 @@ import (
 	// We support gif for reading, but not writing
 	_ "image/gif"
 
-	// TODO: vendor rez
-	"github.com/bamiaux/rez"
+	"github.com/nfnt/resize"
 )
+
+// https://github.com/ajstarks/svgo
 
 // Options represents image options
 type Options struct {
@@ -29,36 +29,35 @@ type Options struct {
 func SaveJpegRepresentations(r io.Reader, options []Options) error {
 
 	if r == nil {
-		return errors.New("Nil reader received in SaveJpegRepresentation")
+		return fmt.Errorf("assets: nil reader received in SaveJpegRepresentation")
 	}
 
 	// Read the image data, if we have a jpeg, convert to png?
 	original, _, err := image.Decode(r)
 	if err != nil {
-		return err
+		return fmt.Errorf("error decoding image:%s", err)
 	}
 
 	// For each option, save a file
 	for _, o := range options {
 
-		//fmt.Printf("Saving image file - %v\n", o)
+		fmt.Printf("assets: saving image file - %v\n", o)
 
-		// Resize this image given the params - this is always in proportion, NEVER stretched
-		// If Square is true we crop to a square
-		resized, err := ResizeImage(original, o.MaxWidth, o.MaxHeight, false)
+		// Resize this image given the params
+		resized, err := resizeImage(original, o.MaxWidth, o.MaxHeight)
 		if err != nil {
-			return err
+			return fmt.Errorf("error resizing file :%s", err)
 		}
 
 		// Write out to the desired file path
 		w, err := os.Create(o.Path)
 		if err != nil {
-			return err
+			return fmt.Errorf("error creating file :%s", err)
 		}
 		defer w.Close()
 		err = jpeg.Encode(w, resized, &jpeg.Options{Quality: o.Quality})
 		if err != nil {
-			return err
+			return fmt.Errorf("error encoding file :%s", err)
 		}
 
 	}
@@ -71,7 +70,7 @@ func SaveJpegRepresentations(r io.Reader, options []Options) error {
 func SavePNGRepresentations(r io.Reader, options []Options) error {
 
 	if r == nil {
-		return errors.New("Nil reader received in SaveJpegRepresentation")
+		return fmt.Errorf("assets: reader received in SaveJpegRepresentation")
 	}
 
 	// Read the image data, if we have a jpeg, convert to png?
@@ -87,7 +86,7 @@ func SavePNGRepresentations(r io.Reader, options []Options) error {
 
 		// Resize this image given the params - this is always in proportion, NEVER stretched
 		// If Square is true we crop to a square
-		resized, err := ResizeImage(original, o.MaxWidth, o.MaxHeight, false)
+		resized, err := resizeImage(original, o.MaxWidth, o.MaxHeight)
 		if err != nil {
 			return err
 		}
@@ -109,18 +108,12 @@ func SavePNGRepresentations(r io.Reader, options []Options) error {
 
 }
 
-// ResizeImage resizes the given image IF it is larger than maxWidth or maxHeight
-func ResizeImage(src image.Image, maxWidth int64, maxHeight int64, square bool) (image.Image, error) {
-	var dst image.Image
+// resizeImage resizes an image
+func resizeImage(src image.Image, maxWidth int64, maxHeight int64) (image.Image, error) {
 
-	// Check the original dimensions first, and bail out if this image is not larger than max dimensions
 	srcSize := src.Bounds().Size()
-	if int64(srcSize.X) < maxWidth && int64(srcSize.Y) < maxHeight {
-		return src, nil
-	}
 
 	// Use the original image dimensions to keep it in pro
-	// Distorting images is a sin of which we are never guilty
 	ratio := float64(maxWidth) / float64(srcSize.X)
 	yRatio := float64(maxHeight) / float64(srcSize.Y)
 	if yRatio < ratio {
@@ -128,29 +121,8 @@ func ResizeImage(src image.Image, maxWidth int64, maxHeight int64, square bool) 
 	}
 
 	// Now adjust desired width and height according to ratio
-	width := float64(srcSize.X) * ratio
-	height := float64(srcSize.Y) * ratio
+	width := uint(float64(srcSize.X) * ratio)
+	height := uint(float64(srcSize.Y) * ratio)
 
-	// Create a new resized image with the desired dimensions and fill it with resized image data
-	// We switch on input image type - is YCbCrSubsampleRatio444 correct?
-	switch src.(type) {
-	case *image.YCbCr:
-		dst = image.NewYCbCr(image.Rect(0, 0, int(width), int(height)), image.YCbCrSubsampleRatio444)
-	case *image.RGBA:
-		dst = image.NewRGBA(image.Rect(0, 0, int(width), int(height)))
-	default:
-		dst = nil
-	}
-
-	err := rez.Convert(dst, src, rez.NewBicubicFilter())
-	// IF we want thumbnails to be square/cropped we could do this
-	// for now we don't need this. We may not even want it for camping?
-	//   err :=  imaging.Thumbnail(srcImage, 100, 100, imaging.Lanczos)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return dst, nil
-
+	return resize.Resize(width, height, src, resize.Bilinear), nil
 }
